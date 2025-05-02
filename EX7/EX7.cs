b684340535289f068b316d6f7953c32b7a0f28ce
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-05-02 15:13:50 -0300

    Segundo exercício de mesa passado ao código.

---
+++ EX7/EX7.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace EX7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2, y = 10;
+            Console.WriteLine("Olá\n");
+            
+            while(x<y)
+            {
+                Console.WriteLine($"X = {x} \nY = {y}\n");
+                x *= 2;
+                y += 1;
+            }
+            Console.ReadLine();
+            }
+        }
+    }
