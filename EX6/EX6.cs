e05f597c0d5091a231761fa05e4e8de4fdbdc76d
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-05-02 11:21:04 -0300

    Teste de mesa da estrutura while passada para a VS Code

---
+++ EX6/EX6.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EX5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int X = 0;
+            while (X < 5)
+            {
+                int Y = X * 3;
+                Console.Write($"{Y} ");
+                X += 1;
+            }
+            Console.WriteLine("\nFIM");
+        }
+    }
+}
