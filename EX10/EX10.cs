a7b7630d202835f0aac00c5db4927e9f4a3fb4c3
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-05-08 17:31:06 -0300

    Exercício que coleta dois valores de entrada separados por espaço e o programa retorna se eles estão em ordem crescente ou decrescente. Se eles estiverem iguais, fim do programa.

---
+++ EX10/EX10.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace EX10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            // Primeira leitura dos números
+            Console.Write("Digite dois números inteiros para avaliação de ordem (separados por espaço): ");
+            string[] vet = Console.ReadLine()!.Split(' ');
+
+            // Validação da entrada
+            if (vet.Length != 2 || !int.TryParse(vet[0], out x) || !int.TryParse(vet[1], out y))
+            {
+                Console.WriteLine("VALOR INVÁLIDO! FAVOR REDIGITAR");
+                return;
+            }
+
+            // Loop para comparar os números até que sejam iguais
+            while (x != y)
+            {
+                if (x < y)
+                {
+                    Console.WriteLine("ORDEM CRESCENTE");
+                }
+                else if (x > y)
+                {
+                    Console.WriteLine("ORDEM DECRESCENTE");
+                }
+
+                // Solicita novos números
+                Console.Write("Digite novamente dois números inteiros (separados por espaço): ");
+                vet = Console.ReadLine()!.Split(' ');
+
+                // Validação da nova entrada
+                if (vet.Length != 2 || !int.TryParse(vet[0], out x) || !int.TryParse(vet[1], out y))
+                {
+                    Console.WriteLine("VALOR INVÁLIDO! FAVOR REDIGITAR");
+                    return;
+                }
+            }
+
+            // Quando x e y forem iguais
+            Console.WriteLine("NÚMEROS IGUAIS! FIM.");
+            Console.ReadLine();
+        }
+    }
+}
