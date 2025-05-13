d92ce6489d02336ef794e36329df72093fabc734
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-05-13 13:24:25 -0300

    Programa que pega a idade que o usuário digitar (válida de 0 a 120) e após ele encerrar o loop, digitando alguma idade absurda, ele calcula a média aritmética dessas idades.

---
+++ EX11/EX11.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace EX11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 0, soma = 0, cont = 0;
+            double media = 0;
+
+            // Primeira leitura FORÇADA: usuário só sai daqui digitando uma idade válida >= 0
+            while (true)
+            {
+                LerIdade(ref idade);
+                if (idade >= 0) break;
+                Console.WriteLine("Não começa com gracinha. Digite uma idade válida.\n");
+            }
+
+            // Loop de coleta de idades
+            while (idade >= 0)
+            {
+                soma += idade;
+                cont++;
+                LerIdade(ref idade);
+            }
+
+            media = (double)soma / cont;
+            Console.WriteLine($"A média das idades é: {media.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.ReadKey();
+        }
+
+        static void LerIdade(ref int idade)
+        {
+            Console.Write("Digite a idade: ");
+            int.TryParse(Console.ReadLine(), out idade);
+        }
+    }
+}
