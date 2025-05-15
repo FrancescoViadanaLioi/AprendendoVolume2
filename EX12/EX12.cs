02019595b08570a0f315c80df7c9b4dd99b96f6b
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-05-15 12:02:11 -0300

    Exercício bem simples que uma variável senha é pré-definida pelo programa e o usuário só vai ter acesso ao sistema quando ele digitar a senha correta.

---
+++ EX12/EX12.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EX12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha = 0, senhaCorreta = 2002;
+
+            while (senha != senhaCorreta)
+            {
+                Console.Write("Digite a senha: ");
+                if (int.TryParse(Console.ReadLine(), out senha) && senha == senhaCorreta)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Senha inválida, digite novamente. Pressione qualquer tecla para continuar.");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+            Console.WriteLine("Bem vindo ao sistema, meu chapa!");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
