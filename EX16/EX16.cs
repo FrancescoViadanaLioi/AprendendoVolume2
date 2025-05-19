9d6795ec45b5b8624fbd89e6cbf2d1e6f0a9125f
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-05-19 15:07:07 -0300

    Novo exercício: Basicamente o mesmo do EX11, apenas algumas alterações de lógica.

---
+++ EX16/EX16.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Globalization;
+
+namespace EX16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 0, contIdades = 0, somaIdades = 0;
+            float media;
+
+            while (true)
+            {
+                Entrada(ref idade);
+                if (idade < 0) break;
+
+                somaIdades += idade;
+                contIdades++;
+            }
+
+            if (contIdades > 0)
+            {
+                media = Saida(somaIdades, contIdades);
+                Console.WriteLine($"A média das idades é {media.ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+            else
+            {
+                Console.WriteLine("Nenhuma idade válida foi digitada.");
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        static void Entrada(ref int idade)
+        {
+            bool verificacao;
+
+            while (true)
+            {
+                Console.Write("Digite a idade (Digite um número negativo para sair do programa): ");
+                verificacao = int.TryParse(Console.ReadLine(), out idade);
+                if (!verificacao || idade > 120)
+                {
+                    Console.WriteLine("Digite uma idade válida.");
+                    Console.ReadKey();
+                    Console.Clear();
+                    continue;
+                }
+                break;
+            }
+        }
+
+        static float Saida(int somaIdades, int contIdades)
+        {
+            return (float)somaIdades / contIdades;
+        }
+    }
+}
