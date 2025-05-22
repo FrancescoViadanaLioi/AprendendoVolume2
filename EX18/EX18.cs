590821e5028f726bc2c8da2deafbf77f74cd8f33
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-05-22 12:23:27 -0300

    Novo Exercício: Programa que faz uma contagem regressiva para o ano novo, utilizando uma biblioteca para fazer o intervalo de tempo.

---
+++ EX18/EX18.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading; // Essa é a biblioteca da contagemm regressiva.
+
+namespace EX18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contagem(); //Chamada do método.
+            Console.WriteLine("Feliz Ano Novo!");
+            Console.ReadKey(); // Pausa para não fechar o programa. 
+        }
+        static void Contagem() // Método criado para criar essa contagem regressiva.
+        {
+            for (int i = 10; i > 0; i--)
+            {
+                Console.Clear(); // Limpa o console a cada segundo.
+                Console.WriteLine($"Tempo para o Ano Novo: {i}");
+                Thread.Sleep(1000); // Um segundo de intervalo a cada contagem.
+            }
+        }
+    }
+}
