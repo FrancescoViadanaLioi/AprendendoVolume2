e1c824fa50deab2ee9e779251802df120b23f9c2
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-05-27 15:35:11 -0300

    EX19: Exercício simples que faz uma tabuada conforme a entrada do usuário.

---
+++ EX19/EX19.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace EX1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = NumTabuada();
+            Tabuada(num);
+            Console.ReadKey();
+        }
+
+        static int NumTabuada()
+        {
+            int num;
+            while (true)
+            {
+                Console.Write("Qual o número você quer na tabuada? ");
+                if (!int.TryParse(Console.ReadLine(), out num) || num <= 0)
+                {
+                    Console.WriteLine("Digite um valor válido.\n(Pressione qualquer tecla para continuar)");
+                    Console.ReadKey();
+                    Console.Clear();
+                    continue;
+                }
+                break;
+            }
+            return num;
+        }
+
+        static void Tabuada(int num)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                int conta = num * i;
+                Console.WriteLine($"{num} * {i} = {conta}");
+            }
+        }
+    }
+}
