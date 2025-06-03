05f93808391ed8dc50d17f225056dfc394c1fc4a
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-06-03 14:49:38 -0300

    EX24 - Update: Novos métodos adicionados à classe de validação, agora os metodos também são usados no Main.

--- EX24/EX24.cs
+++ EX24/EX24.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace EX24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Validacao validador = new Validacao();
+            int n = validador.QntdElem();
+            int[] vetor = validador.Loop(n);
+            validador.MostrarNumeros(vetor);
+            Console.ReadKey();
+        }
+    }
+}
--- EX24/Entrada.cs
+++ EX24/Entrada.cs
@@ -1,18 +1,51 @@
 using System;
 
-class Validacao
+public class Validacao
 {
-    public int QntdElem(int N)
+    public int QntdElem()
     {
+        int N;
         while (true)
         {
             Console.Write("Digite quantos elementos a matriz vai ter: ");
-            if (int.TryParse(Console.ReadLine(), out N) || N > 0) return N;
+            if (int.TryParse(Console.ReadLine(), out N) && N > 0) return N;
             Limpar();
         }
     }
+    public int[] Loop(int N)
+    {
+        int[] vet = new int[N];
 
-    static void Limpar()
+        for (int i = 0; i < N; i++)
+        {
+            while (true)
+            {
+                Console.Write($"DIgite o elemento {i + 1}: ");
+                if (int.TryParse(Console.ReadLine(), out vet[i])) break;
+                Limpar();
+            }
+        }
+        return vet;
+    }
+    public void MostrarNumeros(int[] vet)
+    {
+        Console.WriteLine("Números negativos encontrados:");
+        bool encontrou = false;
+
+        foreach (int num in vet)
+        {
+            if (num < 0)
+            {
+                Console.WriteLine(num);
+                encontrou = true;
+            }
+        }
+        if (!encontrou)
+        {
+            Console.WriteLine("Nenhum número foi encontrado.");
+        }
+    }
+    private void Limpar()
     {
         Console.WriteLine("Valor inválido, digite novamente.\n(Pressione qualquer tecla para continuar)");
         Console.ReadKey();
