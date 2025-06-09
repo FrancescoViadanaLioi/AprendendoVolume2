72b85567dd1487c5b2a46452ca51b9cad49b7db7
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-06-09 14:53:05 -0300

    EX25 - Feat: Adição do método que calcula a média dos elementos dos vetores. Além disso, fiz alterações mínimas no código.

--- EX25/Metodos.cs
+++ EX25/Metodos.cs
@@ -12,18 +12,17 @@ namespace Metodos
             {
                 int Nvet;
                 Console.Write("Digite o tamanho do vetor: ");
-                var valid = Console.ReadLine();
-                if (int.TryParse(valid, out Nvet) && Nvet > 0) return Nvet;
+                if (int.TryParse(Console.ReadLine(), out Nvet) && Nvet > 0) return Nvet;
                 Limpar.LimparTela();
-            }             
+            }
         }
-        public int[] Loop(int Nvet)
+        public int[] LerValoresVetor(int Nvet)
         {
             int[] vetor = new int[Nvet];
 
             for (int i = 0; i < Nvet; i++)
             {
-                while(true)
+                while (true)
                 {
                     Console.Write($"Digite o valor do vetor {i + 1}: ");
                     if (int.TryParse(Console.ReadLine(), out vetor[i])) break;
@@ -31,6 +30,18 @@ namespace Metodos
                 }
             }
             return vetor;
+        }
+        public double CalculoMedia(int[] vetor)
+        {
+            int soma = 0;
+
+            foreach(int valor in vetor)
+            {
+                soma += valor;
+            }
+            double media = (double)soma / vetor.Length;
+
+            return media;
         }
     }
 }
--- EX25/Program.cs
+++ EX25/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Metodos;
 
 namespace EX25
 {
