0d2891f3e412e7f94f344e4756012bbeef2e48e3
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-06-16 17:05:25 -0300

    EX28 - feat: Adição de um método que entrega o pedido de entrada ao usuário, promovendo a legibilidade.

--- EX28/Entrada.cs
+++ EX28/Entrada.cs
@@ -3,42 +3,43 @@ using Miscelania;
 
 namespace Manipulacao
 {
-	public static class EntradaDeDados
-	{
-		public static int[,] CapturarTamMatriz()
-		{
-			int m, n;
+    public static class EntradaDeDados
+    {
+        public static int[,] CapturarTamMatriz()
+        {
+            int m, n;
 
-			while (true)
-			{
-				Console.Write("Digite o número de linhas e de colunas da matriz separadas por espaço: ");
-				string[] vect = Console.ReadLine()!.Split(' ');
-				if (vect.Length == 2 && (int.TryParse(vect[0], out m) && m > 0) && (int.TryParse(vect[1], out n) && n > 0)) return new int[m,n];
-				Limpar.LimparTela();
-			}
-		}
-	}
-	public static class OperarMatriz
-	{
-		public static int[,] PreencherMatriz(int[,] matriz)
-		{
-			for(int i = 0; i < matriz.GetLength(0); i ++)
-			{
-				for(int j = 0; j < matriz.GetLength(1); j++)
-				{
-					while(true)
-					{
-						Console.Write($"Digite o elemento na posição [{i},{j}]: ");
-						if(int.TryParse(Console.ReadLine(), out int valor))
-						{
-							matriz[i, j] = valor;
-							break;
-						}
-						Limpar.LimparTela();
-					}
-				}
-			}
-			return matriz;
-		}
-	}
+            while (true)
+            {
+                Console.Write("Digite o número de linhas e de colunas da matriz separadas por espaço: ");
+                string[] vect = Console.ReadLine()!.Split(' ');
+                if (vect.Length == 2 && (int.TryParse(vect[0], out m) && m > 0) && (int.TryParse(vect[1], out n) && n > 0)) return new int[m, n];
+                Limpar.LimparTela();
+            }
+        }
+    }
+    public static class OperarMatriz
+    {
+        public static void PreencherMatriz(int[,] matriz)
+        {
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    matriz[i, j] = LerElementosPosicao(i, j);
+                }
+            }
+        }
+        private static int LerElementosPosicao(int i, int j)
+        {
+            while(true)
+            {
+                Console.Write($"Digite o elemento da posição [{i},{j}]: ");
+                string? input = Console.ReadLine();
+                if (input != null && int.TryParse(input, out int valor)) return valor;
+                Limpar.LimparTela();
+            }
+        }
+    }
+}
 }
